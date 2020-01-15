dc5b4fdb1cad35058e2243ecdd580aa045f4b723
SWING1993 <10446627@qq.com>
2020-01-15 18:00:24 +0800

    update

---
+++ src/main/java/com/swing/cooltv/CoolTvApplication.java
@@ -0,0 +1,13 @@
+package com.swing.cooltv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolTvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolTvApplication.class, args);
+    }
+
+}
